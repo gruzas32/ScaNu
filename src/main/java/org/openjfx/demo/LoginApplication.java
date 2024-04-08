32e31ff9eb6645fc2ddc08b77eb7453371dbcf10
Kęstutis Klimanskas <kestutis.klimanskas@stud.vilniustech.lt>
2024-04-08 12:56:12 +0300

    testas 2

--- src/main/java/org/openjfx/demo/LoginApplication.java
+++ src/main/java/org/openjfx/demo/LoginApplication.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
+//komentaras
 public class LoginApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
